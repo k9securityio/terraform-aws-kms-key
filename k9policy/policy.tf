7b1a7a865d5c08d53d267572debd04e1aef8398c
Stephen Kuenzli <skuenzli@k9security.io>
2022-03-07 15:05:20 -0700

    Order arns for readability.

--- k9policy/policy.tf
+++ k9policy/policy.tf
@@ -221,8 +221,8 @@ data "aws_iam_policy_document" "resource_policy" {
         concat(
           [local.account_root_user_arn],
           var.allow_administer_resource_arns,
-          var.allow_read_data_arns,
           var.allow_read_config_arns,
+          var.allow_read_data_arns,
           var.allow_write_data_arns,
           var.allow_delete_data_arns,
           var.allow_custom_actions_arns,
